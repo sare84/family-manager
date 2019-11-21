04c3538efbebaf596b4faebd7605fd761d144648
Sascha Reichert <reichert.sascha@googlemail.com>
2019-11-21 19:05:53 +0100

    Added auth module from passport like the description on the nest.js documentation

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { InfoController } from './info/info.controller';
 import { InfoService } from './info/info.service';
 import { LoggerMiddleware } from './logger.middleware';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({ 
-  imports: [],
+  imports: [AuthModule, UsersModule],
   controllers: [InfoController],
   providers: [InfoService],
 })
