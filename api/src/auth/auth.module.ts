4be4c3895061e2e37c2165df2a846f26bd30a695
Sascha Reichert <reichert.sascha@googlemail.com>
2019-11-22 20:25:02 +0100

    added defaultStrategy to settings

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { JwtStrategy } from './jwt.strategy';
 @Module({
   imports: [
     UsersModule, 
-    PassportModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: jwtConstants.secret,
       signOptions: { expiresIn: '1800s' },
