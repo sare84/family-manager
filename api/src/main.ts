6a480cd306a89c65717db54a1004dc72375ff377
Sascha Reichert <reichert.sascha@googlemail.com>
2019-11-23 13:39:36 +0100

    added swagger support

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -6,6 +7,16 @@ async function bootstrap() {
     logger: console,
     cors: true,
   });
+
+  const options = new DocumentBuilder()
+    .setTitle('Family Manager API')
+    .setDescription('Family Manager API description')
+    .setVersion('1.0')
+    .addTag('family', 'manager')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
