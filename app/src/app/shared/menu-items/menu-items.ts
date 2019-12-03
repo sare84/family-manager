30aca19a305812be2101abbd80e67b566975415d
sare <reichert.sascha@googlemail.com>
2019-12-03 20:19:22 +0100

    Changed preinitialised menu items

--- app/src/app/shared/menu-items/menu-items.ts
+++ app/src/app/shared/menu-items/menu-items.ts
@@ -32,31 +32,11 @@ const MENUITEMS = [
     icon: 'content_copy'
   },
   {
-    state: 'material',
-    name: 'Material Ui',
-    type: 'sub',
-    icon: 'bubble_chart',
-    badge: [{ type: 'red', value: '17' }],
-    children: [
-      { state: 'button', name: 'Buttons' },
-      { state: 'cards', name: 'Cards' },
-      { state: 'grid', name: 'Grid List' },
-      { state: 'lists', name: 'Lists' },
-      { state: 'menu', name: 'Menu' },
-      { state: 'tabs', name: 'Tabs' },
-      { state: 'stepper', name: 'Stepper' },
-      { state: 'expansion', name: 'Expansion Panel' },
-      { state: 'chips', name: 'Chips' },
-      { state: 'toolbar', name: 'Toolbar' },
-      { state: 'progress-snipper', name: 'Progress snipper' },
-      { state: 'progress', name: 'Progress Bar' },
-      { state: 'dialog', name: 'Dialog' },
-      { state: 'tooltip', name: 'Tooltip' },
-      { state: 'snackbar', name: 'Snackbar' },
-      { state: 'slider', name: 'Slider' },
-      { state: 'slide-toggle', name: 'Slide Toggle' }
-    ]
-  }
+    state: 'todo',
+    name: 'Todos',
+    type: 'link',
+    icon: 'check_box'
+  },
 ];
 
 @Injectable()
