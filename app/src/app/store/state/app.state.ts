ab4032a9fab0a4ab4ea387452563953c4c795974
sare <reichert.sascha@googlemail.com>
2019-12-06 13:44:21 +0100

    modified app state for todo states

--- app/src/app/store/state/app.state.ts
+++ app/src/app/store/state/app.state.ts
@@ -1,12 +1,16 @@
 import * as auth from '../reducer/auth.reducer';
+import * as todo from '../reducer/todo.reducer';
 import { createFeatureSelector } from '@ngrx/store';
 
 export interface AppState {
-  auth: auth.State;
+  auth: auth.AuthState;
+  todo: todo.TodoState;
 }
 
 export const reducers = {
-  auth: auth.reducer
+  auth: auth.reducer,
+  todo: todo.reducer
 };
 
 export const selectAuthState = createFeatureSelector<AppState>('auth');
+export const selectTodoState = createFeatureSelector<AppState>('todo');
